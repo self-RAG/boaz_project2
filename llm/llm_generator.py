6f47acf52d97ccc564d8317b51f969bfdfd7d2ea
hakuna78 <165583398+hakuna78@users.noreply.github.com>
2026-05-26 18:45:50 +0900

    2.5 -> 2.0으로 바꿈

--- llm/llm_generator.py
+++ llm/llm_generator.py
@@ -89,7 +89,7 @@ class LLMGenerator:
 
             response = (
                 client.models.generate_content(
-                    model="gemini-2.5-flash",
+                    model="gemini-2.0-flash",
 
                     contents=user_content,
 
